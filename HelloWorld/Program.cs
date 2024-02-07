e31e3dbe6ef6b278d49b189795d44e51515581b2
Mateen Ahmed Bhutto <mbhutto.official@gmail.com>
2024-02-07 10:56:00 +0500

    HelloWorld commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Welcome to the C# programming!");
+Console.Write("I am printing");
+Console.Write("from multiple line of Console code!");
+
+/*
+    The difference between .WriteLine() and Write() method is
+    .WriteLine() appends new line after output.
+    ,Write() gives output in single line.
+ */
