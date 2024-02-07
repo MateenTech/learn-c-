bee2db5ad9bdcf69e4dc30a23bec8f1a961a594e
Mateen Ahmed Bhutto <mbhutto.official@gmail.com>
2024-02-07 11:12:36 +0500

    DataTypes

---
+++ DataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Literal String data of type 'string' inside double quotation ("") mark;
+Console.WriteLine("\t'string' data type:");
+Console.Write("Hi i am literal string inside double quotes!");
+Console.WriteLine('\n');
+
+// Character Literal data of type 'char' referred as character (single alphanumeric)
+// and is used inside single quotes ('');
+Console.WriteLine("'char' data type referred as character:");
+Console.Write('M');
+Console.Write('a');
+Console.Write('t');
+Console.Write('e');
+Console.Write('e');
+Console.Write('n');
+Console.WriteLine("");
