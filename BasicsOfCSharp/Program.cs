4acce05099f1cd737ad5feb3719e7b8ad26af331
Mateen Ahmed Bhutto <mbhutto.official@gmail.com>
2024-02-10 13:34:16 +0500

    basicsofcsharp

---
+++ BasicsOfCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace BasicsOfCSharp
+{
+    class Program
+    {
+ 
+        static string GetUserName()
+        {
+            string userName = Console.ReadLine();
+            return userName;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi this is C#! What's your name? ");
+            string fetchUserName = GetUserName();
+
+
+            Console.WriteLine("Hi " + fetchUserName + "! I am so excited to welcome you to Hello World of C#.");
+        }
+    }
+}
